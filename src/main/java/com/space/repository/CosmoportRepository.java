92153abdc98b616902ad11d474c325e9922af81d
LepekhovNikolay <gerb13@yandex.ru>
2019-09-21 14:14:14 +0300

    first commit

---
+++ src/main/java/com/space/repository/CosmoportRepository.java
@@ -0,0 +1,10 @@
+package com.space.repository;
+
+import com.space.model.Ship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CosmoportRepository extends JpaRepository<Ship, Long>, JpaSpecificationExecutor<Ship> {
+}
+
